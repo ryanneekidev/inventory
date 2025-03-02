0950469031d6371c82e2e3bac3b6769667c1d3a9
Rayan Mamhoud <kiraclover@protonmail.com>
2025-03-02 07:40:37 +0000

    Use items router in root app

--- app.js
+++ app.js
@@ -5,10 +5,15 @@ const app = express();
 // Dotenv setup
 require('dotenv').config();
 
+// Import routers
+const itemsRouter = require('./routers/items');
+
 // Routes
 app.get('/', (req, res)=>{
     res.status(200).send('Hello, World!')
 });
+
+app.use('/items', itemsRouter);
 
 // Start server
 app.listen(process.env.PORT || 3000, ()=>{
