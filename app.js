45fb95d80cdb13aeb410ef85a5cc35ceeab01aab
Rayan Mamhoud <kiraclover@protonmail.com>
2025-03-02 06:58:07 +0000

    Setup dotenv and start using environment variables

--- app.js
+++ app.js
@@ -2,12 +2,15 @@
 const express = require('express');
 const app = express();
 
+// Dotenv setup
+require('dotenv').config();
+
 // Routes
 app.get('/', (req, res)=>{
     res.status(200).send('Hello, World!')
 });
 
 // Start server
-app.listen(3000, ()=>{
+app.listen(process.env.PORT || 3000, ()=>{
     console.log('Server started at http://127.0.0.1:3000')
 })
