1283de6c28354e6d68486a983cfda9a17a0d8ec8
Rayan Mamhoud <kiraclover@protonmail.com>
2025-03-02 07:57:15 +0000

    Fix items view

--- routers/items.js
+++ routers/items.js
@@ -2,9 +2,16 @@
 const express = require('express');
 const app = express();
 
+// Node modules/utilities
+const path = require('node:path');
+
+// Setup EJS
+app.set("views", path.join(__dirname, "..", "views"));
+app.set("view engine", "ejs");
+
 // Routes
 app.get('/', (req, res)=>{
-    res.status(200).send('Items page')
+    res.status(200).render('items', {items: items})
 });
 
 // Export router
